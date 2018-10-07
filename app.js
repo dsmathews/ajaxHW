8f20846e76780df0bf8ce8f101415c9d6db9e5eb
Darrell Mathews <smathewsbd@gmail.com>
2018-10-07 17:42:58 -0400

    added code to create input fields, submit and clear buttons, and code to capture text from input field and dynamically create buttons using jQuery.

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-const stockList = []
+const stockList = ['DIS', 'GOOG', 'AAPL','SSNLF'];
+
+const render = function () {
+    $('#stockButtons').empty();
+
+    for (let i = 0; i < stockList.length; i++) {
+        let newButton = $('<button>');
+        newButton.addClass('wookie');
+        newButton.attr('data-name', stockList[i]);
+        newButton.text(stockList[i]);
+        $('#stockButtons').append(newButton);
+    }
+};
+
+const addButton = function(event) {
+    event.preventDefault();
+    const stock =$('#stock-input').val().trim();
+    stockList.push(stock);
+    $('#stock-input').val('');
+    render();
+};
+
+// const validate = function () {
+//     event.preventDefault();
+//     const stock = $('#stock-input').val().trim();
+//     const queryURL = `https://api.iextrading.com/1.0/ref-data/symbols#`;
+//     $.ajax({
+//         url:queryURL,
+//         method: 'GET'
+//     }).then(function(response){
+//         $('#stock-input').text(JSON.stringify(response));
+//     });
+
+//     // if (response.symbol !== stock) {
+//     //     $('#add-stock').hide();
+//     // }
+//     // else {
+//     //     $('#add-stock').show();
+//     // }
+//     console.log(queryURL);
+
+// };
+
+// $('#stock-input').on('keypress', validate);
+$('#add-stock').on('click', addButton);
+render();
+
